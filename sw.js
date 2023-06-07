94d8e56edb9792eb38b2b06a63e5f6abf940180d
Majami29 <89219810+Majami29@users.noreply.github.com>
2023-06-07 21:56:40 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -29,7 +29,7 @@ navigationPreload: true;
     },
   }];
 
-
+//----------------------------------------------------------------------------------------------------------------------------
 buttonInstall.addEventListener('click', async () => {
   // Hide the app provided install promotion
   hideInstallPromotion();
@@ -43,6 +43,42 @@ buttonInstall.addEventListener('click', async () => {
   deferredPrompt = null;
 });
 
+// Initialize deferredPrompt for use later to show browser install prompt.
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+  showInstallPromotion();
+  // Optionally, send analytics event that PWA install promo was shown.
+  console.log(`'beforeinstallprompt' event was fired.`);
+});
+
+window.addEventListener('appinstalled', () => {
+  // Hide the app-provided install promotion
+  hideInstallPromotion();
+  // Clear the deferredPrompt so it can be garbage collected
+  deferredPrompt = null;
+  // Optionally, send analytics event to indicate successful install
+  console.log('PWA was installed');
+});
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+  showInstallPromotion();
+  // Optionally, send analytics event that PWA install promo was shown.
+  console.log(`'beforeinstallprompt' event was fired.`);
+});
+//alllll --------------------------
 
 
 self.addEventListener("message", (event) => {
